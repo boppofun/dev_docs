a070ebeb9f93bf970a4b996b202f34385d2afa13
Ben Hansen <git@benhansen.io>
2026-03-23 18:02:54 +0800

    add "Developer" to site title and fix edit link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -43,8 +43,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/boppofun/dev_docs/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/boppofun/dev_docs/tree/main/",
         },
         blog: false,
         theme: {
@@ -72,7 +71,7 @@ const config: Config = {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: "Boppo",
+      title: "Boppo Developer",
       logo: {
         alt: "Boppo Logo",
         src: "img/favicon.png",
